7508145c028970aa7dcd38d8a36bbc902839d4ad
msifland <msifland@gmail.com>
2019-08-14 12:47:48 -0500

    auto commit

--- backups/caja-copy-path-master/customize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-	cd "$INST_DIR"
-	sed -i "s:('Nautilus', '3.0'):('Caja', '2.0'):g" caja_copy_path.py
-	sed -i 's:Nautilus:Caja:g' caja-copy-path.py caja_copy_path.py translation.py
-	sed -i 's:nautilus:caja:g' caja-copy-path.py caja_copy_path.py translation.py
